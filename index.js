162424866b9c118462cd672126decb33807fa0ed
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2017-05-08 16:48:35 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/* eslint-disable global-require */
+
+module.exports = {
+  createDRFConnector: require('./lib/createDRFConnector'),
+  defaults: require('./lib/defaults'),
+  mw: require('./lib/middleware'),
+};
