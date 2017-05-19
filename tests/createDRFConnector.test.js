3cd4a2b74898664145a72394e9e0eaab3c10ae5a
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2017-05-19 14:14:07 +0200

    Added tests fro createDRFConnector()

---
+++ tests/createDRFConnector.test.js
@@ -0,0 +1,16 @@
+/* globals require, jest, expect, describe, it */
+const createDRFConnector = require('../lib/createDRFConnector');
+
+it('builds the connector', () => {
+  const c = createDRFConnector('/api/v1/test/');
+
+  expect(c).toHaveProperty('create');
+  expect(c).toHaveProperty('read');
+  expect(c).toHaveProperty('create');
+  expect(c).toHaveProperty('delete');
+  expect(c).toHaveProperty('use');
+});
+
+it('requires url path', () => {
+  expect(() => createDRFConnector()).toThrow();
+});
